9d7c4bfe6857c215cede53ae65c5b48d33bab0b2
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2019-01-24 22:01:33 +0700

    remove binaries

--- build.php
+++ build.php
@@ -1,9 +1,11 @@
 #!/usr/bin/env php
 <?php
-$srcRoot = 'src';
-$buildRoot = 'build';
-$phar = new Phar($buildRoot . "/main.phar",FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, "main.phar");
-$phar["main.php"] = file_get_contents($srcRoot . "/main.php");
-$phar["main.class.php"] = file_get_contents($srcRoot . "/main.class.php");
-$phar->setStub( "#!/usr/bin/env php\n".$phar->createDefaultStub("main.php"));
-chmod($buildRoot.'/main.phar',0777);
+define('SRC',__DIR__.'/src');
+define('BUILD',__DIR__.'/build');
+
+$phar = new Phar(BUILD.'/main.phar',FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, 'main.phar');
+
+$phar['main.php'] = file_get_contents(SRC."/main.php");
+$phar['main.class.php'] = file_get_contents(SRC.'/main.class.php');
+$phar->setStub( "#!/usr/bin/env php\n".$phar->createDefaultStub('main.php'));
+chmod(BUILD.'/main.phar',0777);
